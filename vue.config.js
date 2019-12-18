aa9f97c53609cfba210ba7ead89e3c05104e78cc
Zuray David <intheblackworld@gmail.com>
2019-12-18 14:42:02 +0800

    modify

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 let dev_PATH = '/MOEA_People_web/'
-// let dev_PATH = '/MinePeople/'
 let prd_PATH = '/MinePeople'
 const PATH = process.env.NODE_ENV === 'development' ? dev_PATH : prd_PATH
 module.exports = {
